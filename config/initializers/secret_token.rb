d53843e5c6743c2329a8cff0b3d47c7e86006c35
Tomas Morris <tomorris@mines.edu>
2013-02-25 14:44:45 -0700

    Initial commit for Heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cookbook::Application.config.secret_token = '38f2d9173f9781658b12ccf1c1746da4be8997e93747ae88e0a0a9c717cb7c129814c4308c22bf4a0bd5b8a8d2f6f38c30908c672ce56d8f5737860e773df101'
