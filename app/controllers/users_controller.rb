077be1ac51358f46d195b064b49c81bcabdadce2
Tomas Morris <tomorris@mines.edu>
2013-03-25 16:29:17 -0600

    Added registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  skip_before_filter :authorize, only: [:new, :create]
   skip_before_filter :authorize_admin, except: [:destroy]
   
   # GET /users
@@ -46,7 +47,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to users_url, notice: "User #{@user.name} was successfully created." }
+        format.html { redirect_to login_url, notice: "User #{@user.name} was successfully created." }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
