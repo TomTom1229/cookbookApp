22fe196669a5496302e624175262d04ab2a747d4
Tomas Morris <tomorris@mines.edu>
2013-04-23 22:02:55 -0600

    Fixed an issue with null in the book field

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,9 +4,10 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :ingredients
   has_and_belongs_to_many :categories
   has_many :ingredients_recipes
-  accepts_nested_attributes_for :ingredients_recipes, :reject_if => lambda { |a| a[:ingredient_id].blank? }, :allow_destroy => true
-  has_many :categories_recipes
   belongs_to :book
+  accepts_nested_attributes_for :ingredients_recipes, :reject_if => lambda { |a| a[:ingredient_id].blank? }, :allow_destroy => true
+  accepts_nested_attributes_for :book, :reject_if => lambda { |a| a[:ingredient_id].blank? }, :allow_destroy => true
+  has_many :categories_recipes
 
   attr_accessible :instructions, :name, :book_id,
    :categories_tokens, :ingredients_recipes, :ingredients_recipes_attributes,
